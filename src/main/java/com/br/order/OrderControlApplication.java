7f0b65fab3251bceda2c969485a8942fc055e7c9
Luiz Carlos Menezes Santos <luiz.santos@paygo.com.br>
2021-11-23 18:49:36 -0300

    Projeto criado.

---
+++ src/main/java/com/br/order/OrderControlApplication.java
@@ -0,0 +1,13 @@
+package com.br.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderControlApplication.class, args);
+	}
+
+}
